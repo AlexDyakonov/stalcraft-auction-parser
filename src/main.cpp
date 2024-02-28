7b2fe8da080a1b3aef55626242e0afe98947c52b
AlexDyakonov <shram.monolit@mail.ru>
2024-02-28 03:13:02 +0300

    Added logger test

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,19 @@
 #include <clickhouse/client.h>
 #include "utils/utils.hpp"
 extern char **environ;
-
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 int main()
 {    
+    auto logger = spdlog::basic_logger_mt("logger", "logfile.log");
+    spdlog::set_default_logger(logger);
+
+    spdlog::info("Это информационное сообщение");
+    spdlog::warn("Это предупреждение");
+    spdlog::error("Это сообщение об ошибке");
+
+
     utils::loadEnvVariables(".env");
 
     std::cout << "Hello" << std::endl;
